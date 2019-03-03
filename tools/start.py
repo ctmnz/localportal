23dcefbf42b2f2434a7c3fb9e2fa87a3477df162
Daniel Stoinov <daniel.stoinov@gmail.com>
2019-03-03 01:04:10 +0000

    *

--- tools/start.py
+++ tools/start.py
@@ -1,10 +1,20 @@
 import docker
+import sys
+
+app_container_name="pylocalportal"
 
 
 
 def start_my_localportal():
     client = docker.from_env()
-    client.containers.run("ctmnz/localportal", command=None, detach=True, ports={'8080/tcp': 8088}, name="pydocker")
+
+    running_containers = client.containers.list()
+    for i in running_containers:
+        if i.name == app_container_name:
+            sys.exit("The container is currnetly running! Aborting the operation.")
+		
+
+    client.containers.run("ctmnz/localportal", command=None, detach=True, ports={'8080/tcp': 8088}, name=app_container_name)
 
 start_my_localportal()
 
